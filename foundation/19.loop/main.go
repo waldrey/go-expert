29488d3207a7799c90dc9c0c84ca48eb538c6f58
Waldrey <waldrey22@gmail.com>
2024-05-27 21:44:20 -0300

    feat: adding code from foundation classes part iv

---
+++ foundation/19.loop/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	// for i:= 0; i<10; i++ {
+	// 	println(i)
+	// }
+
+	// numbers := []string{"um", "dois", "tres"}
+	// for k, v := range numbers {
+	// 	println(k, v)
+	// }
+
+	// i := 0
+	// for i < 10 {
+	// 	println(i)
+	// 	i++
+	// }
+
+	for {
+		println("Hello World!")
+	}
+}
---
+++ foundation/20.conditionals/main.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+	a := 4
+	// b := 2
+	// c := 3
+
+	switch a {
+	case 1:
+		println("a")
+	case 2:
+		println("b")
+	case 3:
+		println("c")
+	default:
+		println("DEFAULT")
+	}
+
+	// if a > b || c > a {
+	// 	println("a > b && c > a")
+	// }
+
+	// if a > b {
+	// 	println(a)
+	// } else {
+	// 	println(b)
+	// }
+}
