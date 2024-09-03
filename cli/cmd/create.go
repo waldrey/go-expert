55a932b610a3bc843091f0a294e57a7759a51c31
Waldrey <waldrey22@gmail.com>
2024-09-03 20:12:51 -0300

    feat: adding code from cli

---
+++ cli/cmd/create.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/waldrey/go-expert/cli/internal/database"
+)
+
+func newCreateCmd(categoryDb database.Category) *cobra.Command {
+	return &cobra.Command{
+		Use:   "create",
+		Short: "A brief description of your command",
+		Long:  `A longer description that spans multiple lines and likely contains examples`,
+		RunE:  runCreate(categoryDb),
+	}
+}
+
+func init() {
+	createCmd := newCreateCmd(getCategoryDB(getDB()))
+	categoryCmd.AddCommand(createCmd)
+	createCmd.Flags().StringP("name", "n", "", "Category name")
+	createCmd.Flags().StringP("description", "d", "", "Category description")
+	createCmd.MarkFlagsRequiredTogether("name", "description")
+}
+
+func runCreate(categoryDb database.Category) RunEFunc {
+	return func(cmd *cobra.Command, args []string) error {
+		name, _ := cmd.Flags().GetString("name")
+		description, _ := cmd.Flags().GetString("description")
+
+		_, err := categoryDb.Create(name, description)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Category created")
+		return nil
+	}
+}
